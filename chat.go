b3d2d83181ae0b3502a376109ef6d403698b42d0
Pino' Surace <pino.surace@live.it>
2025-11-13 21:30:37 +0100

    Add file type

--- chat.go
+++ chat.go
@@ -86,12 +86,19 @@ type ChatMessagePartType string
 const (
 	ChatMessagePartTypeText     ChatMessagePartType = "text"
 	ChatMessagePartTypeImageURL ChatMessagePartType = "image_url"
+	ChatMessagePartTypeFile     ChatMessagePartType = "file"
 )
 
 type ChatMessagePart struct {
 	Type     ChatMessagePartType  `json:"type,omitempty"`
 	Text     string               `json:"text,omitempty"`
 	ImageURL *ChatMessageImageURL `json:"image_url,omitempty"`
+	File     *ChatMessageFile     `json:"file,omitempty"`
+}
+
+type ChatMessageFile struct {
+	Filename string `json:"filename,omitempty"`
+	FileData string `json:"file_data,omitempty"`
 }
 
 type ChatCompletionMessage struct {
